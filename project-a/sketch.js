1da8c5f0bb45e6d69a3a3ed19c171bced9a11efd
bc3358 <bc3358@nyu.edu>
2026-03-06 12:06:25 +0800

    Update sketch.js

--- project-a/sketch.js
+++ project-a/sketch.js
@@ -1,16 +1,3 @@
-/*
-Template for IMA's Creative Coding Lab 
-
-Project A: Generative Creatures
-CCLaboratories Biodiversity Atlas 
-*/
-
-let x;
-let y;
-let R = 100;
-let vNumber = 200;
-let smoothness = 0.5;
-
 function setup() {
     let canvas = createCanvas(800, 500);
     canvas.id("p5-canvas");
@@ -18,25 +5,87 @@ function setup() {
 }
 
 function draw() {
-    background(0);
-    noStroke();
+    background(10, 40, 90);
+
+    jelly(100, 120, 0.8, color(180, 120, 200, 180), 1);
+    jelly(250, 250, 1.3, color(150, 100, 220, 180), 0.6);
+    jelly(350, 180, 0.6, color(200, 150, 255, 180), 1.5);
+}
+
+function drawCreature(x, y) {
     push();
-    translate(width / 2, height / 2);
-    rotate(sin(frameCount * 0.01));
-    beginShape();
-    for (let i = 0; i < TWO_PI; i += TWO_PI / vNumber) {
-        let offset = noise(
-            smoothness * sin(i),
-            smoothness * cos(i),
-            frameCount * 0.01
-        );
-        offset = map(offset, 0, 1, -R * 0.2, R * 0.2);
-        let radius = R + offset;
-        x = radius * cos(i);
-        y = radius * sin(i);
-        curveVertex(x, y);
-    }
-    endShape(CLOSE);
+    translate(x, y);
+
+    drawBody();
+    drawTentacles();
+
+    // YOUR CODE GOES HERE
+    // introduce additional functions
+    // for parts of your creature that
+    // are repeated, and call them from
+    // here
+
     pop();
 }
 
+function drawBody(col, speed) {
+    push();
+    noStroke();
+
+    let breathe = map(sin(frameCount / 30 * speed), -1, 1, -5, 5);
+
+    fill(col);
+    ellipse(0, 20 + breathe, 180, 120);
+
+    fill(255, 255, 255, 70);
+    ellipse(-35, 10 + breathe, 40, 25);
+
+    pop();
+}
+
+
+function drawTentacles(speed) {
+    stroke(160, 100, 200, 180);
+    strokeWeight(3);
+    noFill();
+
+    for (let i = -70; i <= 70; i += 20) {
+        drawTentacle(i, speed);
+    }
+}
+
+function drawTentacle(xOffset, speed) {
+    push();
+    translate(xOffset, 60);
+
+    beginShape();
+
+    for (let y = 0; y <= 120; y += 15) {
+        let xWave = map(
+            sin(frameCount / 20 * speed + y * 0.2 + xOffset),
+            -1, 1,
+            -10, 10
+        );
+        vertex(xWave, y);
+    }
+
+    endShape();
+    pop();
+}
+
+
+function jelly(cx, cy, size, col, speed) {
+    push();
+
+    let moveX = cx + sin(frameCount / 60 * speed) * 30;
+
+    let moveY = cy - (frameCount * 0.2 * speed) % height;
+
+    translate(moveX, moveY);
+    scale(size);
+
+    drawBody(col, speed);
+    drawTentacles(speed);
+
+    pop();
+}
