b7dc8d7f16bc059f1e038ad76804fd39c550d608
bc3358 <bc3358@nyu.edu>
2026-03-10 11:08:32 +0800

    C

--- project-a/sketch.js
+++ project-a/sketch.js
@@ -1,15 +1,14 @@
 function setup() {
     let canvas = createCanvas(800, 500);
+
     canvas.id("p5-canvas");
     canvas.parent("p5-canvas-container");
 }
 
 function draw() {
-    background(10, 40, 90);
-
-    jelly(100, 120, 0.8, color(180, 120, 200, 180), 1);
-    jelly(250, 250, 1.3, color(150, 100, 220, 180), 0.6);
-    jelly(350, 180, 0.6, color(200, 150, 255, 180), 1.5);
+    drawWaterBackground();
+    drawReefs();
+    jelly(400, 250, 0.8, color(180, 120, 220, 180), 1);
 }
 
 function drawCreature(x, y) {
@@ -26,6 +25,19 @@ function drawCreature(x, y) {
     // here
 
     pop();
+}
+
+function drawWaterBackground() {
+    for (let y = 0; y < height; y++) {
+        let inter = map(y, 0, height, 0, 1);
+        let c = lerpColor(
+            color(80, 170, 220),
+            color(5, 20, 60),
+            inter
+        );
+        stroke(c);
+        line(0, y, width, y);
+    }
 }
 
 function drawBody(col, speed) {
@@ -77,15 +89,44 @@ function drawTentacle(xOffset, speed) {
 function jelly(cx, cy, size, col, speed) {
     push();
 
-    let moveX = cx + sin(frameCount / 60 * speed) * 30;
+    let moveX = cx + sin(frameCount / 100 * speed) * 30;
+    moveX = constrain(moveX, 120, width - 120);
 
-    let moveY = cy - (frameCount * 0.2 * speed) % height;
+    let moveY = cy + sin(frameCount / 140 * speed) * 120;
+    moveY = constrain(moveY, 120, height - 120);
+
+    let d = dist(mouseX, mouseY, moveX, moveY);
+
+    let proximity = map(d, 200, 0, 0, 1, true);
+    let panic = proximity * proximity;
+
+    if (d < 40) {
+        panic = 1;
+    }
+
+    let tentacleSpeed = speed + panic * 4;
+    let intensity = 1 + panic * 4;
 
     translate(moveX, moveY);
     scale(size);
 
     drawBody(col, speed);
-    drawTentacles(speed);
+    drawTentacles(tentacleSpeed, intensity);
 
     pop();
+}
+function drawReefs() {
+    noStroke();
+
+    fill(210, 140, 90);
+    ellipse(120, height - 40, 180, 100);
+
+    fill(230, 170, 120);
+    ellipse(180, height - 50, 140, 90);
+
+    fill(180, 120, 80);
+    ellipse(width - 120, height - 30, 200, 110);
+
+    fill(200, 150, 100);
+    ellipse(width - 200, height - 50, 150, 90);
 }
